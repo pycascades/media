ec37aa0b9f547e34da12952bcdcc2ea81f52692a
Ben Berry <bengerman@gmail.com>
2023-03-05 23:25:19 -0800

    Fix user & arg passing
    move user mapping to build time to fix inkscape stuff
    switch to correct entrypoint format so args can be passed

--- signs/docker/entrypoint.sh
+++ signs/docker/entrypoint.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
+set -ex
 
 output_dir=/work/output
 mkdir -p ${output_dir}
 
 function render() {
+    filename=$1
     no_extension="${filename%.*}"
     intermediate_png="${output_dir}/${no_extension}.png"
     final="${output_dir}/${no_extension}.pdf"
     # we could probably technically do all of this in imagemagick (`convert` below),
     # but I found inkscape's export a lot easier to control size consistently.
-    inkscape --export-type=png --export-dpi=300 --export-filename="${output_dir}/${no_extension}.png" "${file}"
+    inkscape --export-type=png --export-dpi=300 --export-filename="${output_dir}/${no_extension}.png" /work/svgs/"${filename}"
     # the debug is because the disk cache seems not to work without it
     convert -debug cache "${intermediate_png}" -colorspace CMYK ${final}
 }
-
 
 if [[ "$#" -gt 0 ]]; then
     render "$(basename $1)"
 else
     for file in $(find /work/svgs -name '*.svg'); do
         filename="$(basename ${file})"
-        render filename
+        render $filename
     done
 fi
